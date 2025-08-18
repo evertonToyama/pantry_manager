f22ba68743409c5560b51f425095f3fd9f762b41
Everton Toyama <everton.toyama@gmail.com>
2025-08-18 12:05:22 -0300

    Failures added

---
+++ lib/core/failures/failures.dart
@@ -0,0 +1,19 @@
+import 'package:equatable/equatable.dart';
+
+abstract class Failure extends Equatable {
+  final String message;
+  final int statusCode;
+
+  const Failure({
+    required this.message,
+    required this.statusCode,
+  });
+
+  @override
+  List<Object?> get props => [statusCode, message];
+
+  @override
+  String toString() {
+    return "$statusCode - Error: $message";
+  }
+}
