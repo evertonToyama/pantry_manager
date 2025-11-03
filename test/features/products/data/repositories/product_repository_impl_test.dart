5269627cfaec987279940854f003eae410f75339
evertonToyama <everton.toyama@gmail.com>
2025-11-03 11:04:03 -0300

    Product data repository tests created

---
+++ test/features/products/data/repositories/product_repository_impl_test.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group("create product", () {
+    test("success", () async {});
+    test("failw", () async {});
+  });
+
+  group("delete product", () {
+    test("success", () async {});
+    test("failw", () async {});
+  });
+
+  group("get all products", () {
+    test("success", () async {});
+    test("failw", () async {});
+  });
+
+  group("get product", () {
+    test("success", () async {});
+    test("failw", () async {});
+  });
+
+  group("update product info", () {
+    test("success", () async {});
+    test("failw", () async {});
+  });
+
+  group("update product pantry", () {
+    test("success", () async {});
+    test("failw", () async {});
+  });
+}
